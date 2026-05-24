909c56c8283961d99fe203209aed1f887608f91b
Drdrerer <eggge6e@gmail.com>
2026-05-24 12:46:59 +0300

    Delete lab1 directory

--- lab1/main.py
+++
@@ -1,5 +0,0 @@
-from social_cli import SocialCLI
-
-if __name__ == "__main__":
-    app = SocialCLI()
-    app.run()
--- lab1/message.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-
-class Message:
-    def __init__(self, sender: str, text: str) -> None:
-        self.sender = sender
-        self.text = text
-        self.timestamp = datetime.now()
